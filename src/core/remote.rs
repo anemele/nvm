043a3f8b1c4c06eafd1507db0e103d76775cc54e
Jason Swift <anemele@outlook.com>
2024-03-18 02:22:12 +0800

    update: use `tinyget` instead of `reqwest`

--- src/core/remote.rs
+++ src/core/remote.rs
@@ -1,12 +1,12 @@
 use super::get_node_url;
 use super::map_versions;
 use super::{VersionMap, VersionVec};
-use reqwest::{blocking::Client, Result};
 use serde::Deserialize;
 use serde_json::Value;
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io::Write;
 use std::path::Path;
+use tinyget;
 
 #[derive(Debug, Deserialize)]
 pub struct Index {
@@ -25,28 +25,22 @@ pub struct Index {
 
 pub type Indexes = Vec<Index>;
 
-fn get_index() -> Result<Indexes> {
+fn get_index() -> Option<Indexes> {
     let url = get_node_url("index.json");
-    Client::new()
-        .get(url)
-        .header("User-Agent", "NVM Client")
-        .send()?
-        .json::<Indexes>()
-}
-
-#[test]
-fn test_get_index() {
-    match get_index() {
-        Ok(indexes) => {
-            assert!(indexes.len() > 0);
-            assert!(indexes[0].files.len() > 0);
-        }
-        Err(e) => assert_eq!("", e.to_string()),
-    }
+    let Ok(res) = tinyget::get(url)
+        .with_header("User-Agent", "NVM Client")
+        .send()
+    else {
+        return None;
+    };
+    let Ok(i) = serde_json::from_slice::<Indexes>(res.as_bytes()) else {
+        return None;
+    };
+    Some(i)
 }
 
 pub fn get_map_versions() -> Option<(VersionMap, VersionVec)> {
-    let Ok(indexes) = get_index() else {
+    let Some(indexes) = get_index() else {
         return None;
     };
 
@@ -59,28 +53,23 @@ pub fn get_map_versions() -> Option<(VersionMap, VersionVec)> {
 }
 
 pub fn get_dist(url: &str, path: &Path) -> bool {
-    let mut res = match Client::new()
-        .get(url)
-        .header("User-Agent", "NVM Client")
+    let res = match tinyget::get(url)
+        .with_header("User-Agent", "NVM Client")
         .send()
     {
         Err(_) => return false,
         Ok(res) => res,
     };
 
-    if !res.status().is_success() {
+    if res.status_code >= 300 {
         return false;
     }
 
-    let mut buf = vec![];
-    match res.read_to_end(&mut buf) {
-        Err(_) => return false,
-        Ok(_) => match File::create(&path) {
+    match File::create(&path) {
+        Err(_) => false,
+        Ok(mut file) => match file.write(res.as_bytes()) {
+            Ok(size) => size > 0,
             Err(_) => false,
-            Ok(mut file) => match file.write(buf.as_slice()) {
-                Ok(size) => size > 0,
-                Err(_) => false,
-            },
         },
     }
 }
